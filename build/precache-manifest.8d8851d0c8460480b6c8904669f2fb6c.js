b542908ab1fa6de3c307f9a34c88f5d9477de0ee
malbingi <mateuszalbingier53@gmail.com>
2020-10-12 18:55:55 +0200

    added front

---
+++ build/precache-manifest.8d8851d0c8460480b6c8904669f2fb6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633d3b63859578a536c998178bdc4e43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58f427b2c8bc2cdd4495",
+    "url": "/static/js/2.57ecb691.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57ecb691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e187086874d1db903d",
+    "url": "/static/js/main.49a73af6.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
---
+++ build/precache-manifest.f09ee2f32bce355d8670443b35654011.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5480b6855ad6457133d611b8f298968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58f427b2c8bc2cdd4495",
+    "url": "/static/js/2.57ecb691.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57ecb691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432083069acf602af0dc",
+    "url": "/static/js/main.83e17dea.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
