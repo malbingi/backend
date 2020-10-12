84e58b88e0c56d5644cf04630a4e80af743c9e96
malbingi <mateuszalbingier53@gmail.com>
2020-10-12 22:24:12 +0200

    updte

--- index.js
+++ index.js
@@ -1,99 +1,10 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const Note = require("./models/note");
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const cors = require("cors");
+const server = http.createServer(app)
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static("build"));
-
-app.get("/api/notes", (req, res) => {
-  Note.find({}).then((notes) => {
-    res.json(notes);
-  });
-});
-
-app.get("/api/notes/:id", (req, res) => {
-  Note.findById(req.params.id)
-    .then((note) => {
-      if (note) {
-        res.json(note);
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).send({ error: "malformed id" });
-    });
-});
-
-app.delete("/api/notes/:id", (req, res, next) => {
-  Note.findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-app.post("/api/notes", (request, response) => {
-  const body = request.body;
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  });
-
-  note
-    .save()
-    .then(savedNote => savedNote.toJSON())
-    .then(savedAndFormattedNote => {
-      response.json(savedAndFormattedNote)
-    })
-    .catch(error => next(error))
-});
-
-app.put("/api/notes/:id", (req, res, next) => {
-  const body = req.body;
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
-    .then((updatedNote) => {
-      res.json(updatedNote);
-    })
-    .catch((error) => next(error));
-});
-
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
-};
-
-app.use(unknownEndpoint);
-
-const errorHandler = (error, req, res, next) => {
-  console.log(error.message);
-
-  if (error.name === "CastError" && error.kind == "ObjectId") {
-    return res.status(400).send({
-      error: "malformed id",
-    });
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({error: error.message})
-  }
-
-  next(error);
-};
-
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log("Server running on port ${PORT}");
-});
+server.listen(config.PORT, () => {
+    logger.info('Server running on port ${config.PORT}')
+})
--- models/note.js
+++ models/note.js
@@ -1,33 +1,18 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-}).then(res => {
-    console.log('connected to MongoDB')
-}).catch((error) => {
-    console.log('error connecting to MongoDB', error.message)
-})
-
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
         minlength: 1,
-        required: true
+        required: true,
     },
     date: {
         type: Date,
-        required: true
+        required: true,
     },
     important: {
-        type:Boolean
-    }
+        type: Boolean,
+    },
 })
 
 noteSchema.set('toJSON', {
